75cb956f81a6025a45fdccf9591836bab4435737
Zak Goldberg <zak.goldberg@gmail.com>
2024-10-25 12:56:31 -0700

    Updated the front page with new cards.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Navbar, Card } from "flowbite-react";
+import { Navbar, Card, Timeline } from "flowbite-react";
 import { Laptop } from "lucide-react";
 
 function App() {
@@ -20,16 +20,35 @@ function App() {
         </Navbar>
         </div>
       </div>
-      <section className="container mx-auto pt-20 pb-5 px-4 max-h-10">
-        <div className = "flex flex-col space-y-4 md:flex-row md:space-x-4 md:space-y-0">
+      <section className="container mx-auto pt-20 pb-5 px-4">
+        <div className = "flex flex-col justify-between max-h-72 space-y-4 md:flex-row md:space-x-4 md:space-y-0">
           <Card className="flex-1 w-full md:w-1/2 max-w-md">
             <div className="h-full flex flex-col justify-center">
               <h4 className="text-5xl font-bold mb-2">Welcome 👋🏼</h4>
               <h4 className="text-4xl">My Name is <span className="font-semibold">Zak</span></h4>
             </div>
           </Card>
-          <Card imgAlt="Zak headshot" imgSrc="/zjg_headshot.jpg"  className="flex-1 w-full md:w-1/2 max-w-lg p-0">
-            <p>I am an SF Bay Area based software professional with 9+ years of experience working at Amazon and AWS. I am focused on backend development with the Express, Node, and PostgreSQL stack and am excited to learn new languages and technologies.</p>
+          <Card className="flex-1 w-full md:w-1/2 max-w-lg p-0">
+            <img className="h-auto w-auto object-none" src="/zjg_headshot.jpg" />
+          </Card>
+        </div>
+      </section>
+      <section className="container mx-auto py-5 max-h-72 px-4">
+        <div className = "flex flex-col justify-between space-y-4 md:flex-row md:space-x-4 md:space-y-0">
+          <Card className="flex-1 w-full md:w-1/2 max-w-md">
+          <p>I am an SF Bay Area based software professional with 9+ years of experience working at Amazon and AWS. I am focused on backend development with the Express, Node, and PostgreSQL stack and am excited to learn new languages and technologies.</p>
+          </Card>
+          <Card>
+            <Timeline>
+              <Timeline.Item>
+                <Timeline.Point />
+                <Timeline.Content>
+                  <Timeline.Time>February 2022</Timeline.Time>
+                  <Timeline.Title>Software Development Manager - AWS</Timeline.Title>
+                  <Timeline.Body>Managed a team of 12 SDEs</Timeline.Body>
+                </Timeline.Content>
+              </Timeline.Item>
+            </Timeline>
           </Card>
         </div>
       </section>
