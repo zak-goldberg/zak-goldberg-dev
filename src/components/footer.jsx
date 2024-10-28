8da4b8ffdd4dbb8ed0666083ee49a3affd3805a6
Zak Goldberg <zak.goldberg@gmail.com>
2024-10-28 11:53:00 -0700

    Updated Footer with repo link

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,10 +4,16 @@ const MyFooter = () => {
   return (
     <section id="footer-container" className="container mx-auto px-4 py-2">
       <div className="bg-[#1a1e2e] py-2 border rounded justify-evenly">
-        <Footer className="bg-[#1a1e2e] px-2">
+        <Footer className="bg-[#1a1e2e] px-2 flex flex-row justify-between align-middle">
           <span className="text-white">
             Copyright © 2024 Zak Goldberg. All Rights Reserved.
           </span>
+          <a
+            href="https://github.com/zak-goldberg/zak-goldberg-dev/"
+            target="_blank"
+          >
+            <span className="text-white">Repo For This Page</span>
+          </a>
         </Footer>
       </div>
     </section>
