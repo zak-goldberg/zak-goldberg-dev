7dbc641d2270b7414df36d53388a5b45e9c5ec9c
Zak Goldberg <zak.goldberg@gmail.com>
2024-10-29 11:41:22 -0700

    Updated error page.

--- src/components/error-page.jsx
+++ src/components/error-page.jsx
@@ -1,16 +1,22 @@
 import { useRouteError } from "react-router-dom";
+import MyNavbar from "./navbar";
+import MyFooter from "./footer";
 
 export default function ErrorPage() {
   const error = useRouteError();
   console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
+    <>
+      <MyNavbar />
+      <div id="error-page">
+        <div className="pt-20 min-h-[90vh] flex flex-col space-y-10 justify-center items-center">
+          <h1 className="text-white text-3xl md:text-5xl">
+            404: Page Not Found 🕵🏻
+          </h1>
+        </div>
+      </div>
+      <MyFooter />
+    </>
   );
 };
